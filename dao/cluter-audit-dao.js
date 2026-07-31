57b7a8547e97e5ec0b5e6cd00ed6d139851f9771
Hahinika Dilrukshi <ghashinika2002@gmail.com>
2026-07-31 16:14:48 +0530

    bug fix

--- dao/cluter-audit-dao.js
+++ dao/cluter-audit-dao.js
@@ -26,7 +26,6 @@ exports.getclusterVisits = async (feildauditId) => {
             cp.clusterId,
             cp.id AS certificationpaymentId,
             fcf.id AS farmclusterfarmersId,
-
             CASE 
               WHEN SUM(
                 CASE 
@@ -36,25 +35,23 @@ exports.getclusterVisits = async (feildauditId) => {
                   ELSE 0 
                 END
               ) > 0 THEN 'Opend'
+              WHEN COUNT(js.id) > 0 THEN 'Opend'
               ELSE 'Start'
             END AS jobStatus,
-
             fau.sheduleDate
-
           FROM feildaudits AS fau
           LEFT JOIN feildauditcluster AS fauc ON fauc.feildAuditId = fau.id
           LEFT JOIN farms AS f ON f.id = fauc.farmId
           LEFT JOIN certificationpayment AS cp ON fau.paymentId = cp.id
           LEFT JOIN users AS ps ON f.userId = ps.id
           LEFT JOIN farmcluster AS fc ON cp.clusterId = fc.id
-                LEFT JOIN farmclusterfarmers AS fcf ON fcf.farmId = f.id AND  fcf.clusterId = cp.clusterId
+          LEFT JOIN farmclusterfarmers AS fcf ON fcf.farmId = f.id AND fcf.clusterId = cp.clusterId
           LEFT JOIN slavequestionnaire AS sq ON fcf.id = sq.clusterFarmId
           LEFT JOIN slavequestionnaireitems AS slqi ON slqi.slaveId = sq.id
-
+          LEFT JOIN jobsuggestions AS js ON js.slaveId = sq.id
           WHERE fauc.feildAuditId = ?
           GROUP BY fauc.id, fau.id, f.id, fcf.id, cp.id, ps.id, fc.id
         `;
-
     db.plantcare.query(sql, [feildauditId], (err, results) => {
       if (err) {
         console.error("Database error:", err.message);
