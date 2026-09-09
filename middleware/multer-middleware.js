728bff96bcdd5e95f8bb9d467b34ad23bad3b68f
Hahinika Dilrukshi <ghashinika2002@gmail.com>
2026-09-09 18:45:37 +0530

    bug fix

--- middleware/multer-middleware.js
+++ middleware/multer-middleware.js
@@ -6,11 +6,13 @@ const upload = multer({
   storage: storage,
   limits: { fileSize: 10 * 1024 * 1024 },
   fileFilter: (req, file, cb) => {
-    const filetypes = /jpeg|jpg|png/;
+    const filetypes = /jpeg|jpg|png|webp/;
     const extname = filetypes.test(
       path.extname(file.originalname).toLowerCase(),
     );
-    const mimetype = filetypes.test(file.mimetype);
+    const mimetype =
+      /image\/(jpeg|jpg|png|webp)/.test(file.mimetype) ||
+      filetypes.test(file.mimetype);
 
     if (mimetype && extname) {
       return cb(null, true);
